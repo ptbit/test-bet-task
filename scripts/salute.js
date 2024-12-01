c179fa32c3b15b76253f1fbc3aabf7855a8c52cd
foxstudent103376 <yashopua@gmail.com>
2024-12-01 10:42:34 +0200

    add conditions

--- scripts/salute.js
+++ scripts/salute.js
@@ -2,29 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const salutes = document.querySelectorAll('.salute');
 
   salutes.forEach((salute) => {
-    // Початково ховаємо всі салюти
     gsap.set(salute, { opacity: 0, scale: 0 });
 
     const getRandomOffset = () => {
       const viewportWidth = window.innerWidth;
 
-      // Визначаємо максимальне зміщення як 10% від ширини вікна
       const offset = Math.round(viewportWidth * 0.1);
-      return gsap.utils.random(-offset, offset); // Випадкове значення в межах [-offset, offset]
+      return gsap.utils.random(-offset, offset);
     };
 
     const animateSalute = () => {
-      const randomDelay = gsap.utils.random(2, 10); // Випадкова затримка між циклами
-      const randomX = getRandomOffset(); // Випадкове зміщення по X
-      const randomY = getRandomOffset(); // Випадкове зміщення по Y
+      const randomDelay = gsap.utils.random(2, 10);
+      const randomX = getRandomOffset();
+      const randomY = getRandomOffset();
 
-      // Випадкова тривалість анімації від 1,5 до 3,5 секунд
       const randomDuration = gsap.utils.random(1.5, 3.5);
 
-      // Анімація для кожного салюту
       const timeline = gsap.timeline({
         onComplete: () => {
-          // Після завершення поточної анімації запускаємо її знову через випадкову затримку
           gsap.delayedCall(randomDelay, animateSalute);
         },
       });
@@ -41,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
           {
             scale: 1,
             opacity: 1,
-            duration: randomDuration, // Випадкова тривалість анімації
+            duration: randomDuration,
             ease: 'power1.inOut',
           }
         )
@@ -51,8 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    // Запускаємо анімацію з випадковою стартовою затримкою для кожного салюту
-    const randomInitialDelay = gsap.utils.random(0, 3); // Випадкова стартова затримка
+    const randomInitialDelay = gsap.utils.random(0, 3);
     gsap.delayedCall(randomInitialDelay, animateSalute);
   });
 });
