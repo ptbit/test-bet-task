13c9421083ddd190e1049f27d5c5f7b8cb5703c9
foxstudent103376 <yashopua@gmail.com>
2024-12-02 10:44:16 +0200

    final html

--- scripts/gsap-koala.js
+++ scripts/gsap-koala.js
@@ -4,5 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
     duration: 1.5,
     delay: 1,
     ease: 'power1.inOut',
+    onComplete: () => {
+      setTimeout(() => {
+        gsap.to('.koala', {
+          x: '5%',
+          duration: 1,
+          ease: 'power1.inOut',
+          yoyo: true,
+          repeat: -1,
+          repeatDelay: 10,
+        });
+      }, 5000);
+    },
   });
 });
